cca772589c109327ee8ecbd3259c6fb803bc4284
programmeramandeep <programmeramandeep@gmail.com>
2021-05-17 19:14:12 +0530

    semantic ui & routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+// Css
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+// Components
+import MenuBar from "./components/MenuBar";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Router>
+            <Container>
+                <MenuBar />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+            </Container>
+        </Router>
     );
 };
 
