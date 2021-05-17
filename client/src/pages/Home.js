f0deda33dcbb4d9a9b215e2d09020335b95e004a
programmeramandeep <programmeramandeep@gmail.com>
2021-05-17 21:12:21 +0530

    displaying posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,36 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { Grid } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 const Home = () => {
+    const {
+        loading,
+        data: { getPosts: posts },
+    } = useQuery(FETCH_POSTS_QUERY);
+
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Grid columns={3}>
+            <Grid.Row className="page-title">
+                <h1>Recent Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+                {loading ? (
+                    <h1>Loading posts...</h1>
+                ) : (
+                    posts &&
+                    posts.map((post) => (
+                        <Grid.Column
+                            key={post.id}
+                            style={{ marginBottom: "1.5rem" }}
+                        >
+                            <PostCard post={post} />
+                        </Grid.Column>
+                    ))
+                )}
+            </Grid.Row>
+        </Grid>
     );
 };
 
